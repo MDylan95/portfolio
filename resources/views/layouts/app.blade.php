d13bb78a93300f9bfca109b63dca457a2ab4ad17
Mac-Dylan KACOU <macdylankacou2000@gmail.com>
2026-03-03 22:07:50 +0000

    Use new favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>@yield('title', 'Bokame Mac-Dylan KACOU')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo1.png') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     <link
       rel="stylesheet"
